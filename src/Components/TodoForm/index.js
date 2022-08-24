5398482a2eceda7642ca8f4c58972c53cfe15f43
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2022-08-24 15:41:53 +0700

    updating todo and delete checked todo

--- src/Components/TodoForm/index.js
+++ src/Components/TodoForm/index.js
@@ -1,29 +1,48 @@
 import React, { useState, useRef } from 'react'
-import { useDispatch } from 'react-redux';
-import { addTodo } from '../../store/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, todoInputValue, updateTodo, stateEdit, deleteChecked } from '../../store/reducer';
 
 const TodoForm = () => {
     const inputRef = useRef();
     const dispatch = useDispatch();
-    const [inputValue, setInputValue]= useState('');
+    const edit = useSelector(state => state.todosReducer.isEdited);
+    const todoInput = useSelector(state => state.todosReducer.todoInput);
+    const todoId = useSelector(state => state.todosReducer.todoId);
+    const [inputValue, setInputValue] = useState('');
 
-    const handleChange = (e)  =>{
+    const handleChange = (e) => {
         setInputValue(e.target.value)
     }
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(inputValue);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
         dispatch(addTodo(inputValue));
+
         setInputValue('')
         inputRef.current.focus();
     }
+
+    const handleUpdate = (e)=>{
+        e.preventDefault();
+        dispatch(updateTodo({id: todoId, title: todoInput}))
+        dispatch(stateEdit());
+    }
+    const handleDeleteChecked = () =>{
+        dispatch(deleteChecked())
+    }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input ref={inputRef} value={inputValue} type='text' onChange={handleChange} />
-                <input type='submit' value='Add' />
+            <form onSubmit={edit? handleUpdate : handleSubmit }>
+                {
+                    edit ?
+                        <input value={todoInput} type='text' onChange={(e) => dispatch(todoInputValue(e.target.value))} />
+                        :
+                        <input ref={inputRef} value={inputValue} type='text' onChange={handleChange} />
+
+                }
             </form>
+            <input type='submit' value={edit? 'Update':'Add'}/>
+            <button className='delete-btn' onClick={handleDeleteChecked}>Delete checked</button>
         </div>
     )
 }
--- src/Components/Todos/index.js
+++ src/Components/Todos/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { deleteTodo, markCompleted, todoSelector } from '../../store/reducer'
+import { deleteTodo, markCompleted, selectedTodo, stateEdit, todoSelector } from '../../store/reducer'
 import TodoForm from '../TodoForm'
 
 const Todos = () => {
    const todo = useSelector(todoSelector);
+   const edit = useSelector(state => state.todosReducer.isEdited);
    const dispatch = useDispatch();
     const handleChangeChecked = (todoId) =>{
        dispatch(markCompleted(todoId))
     }
     const handleDelete = id =>{
         dispatch(deleteTodo(id))
+    }
+    const handleSelected = (id, title) =>{
+        dispatch(selectedTodo({id, title}));
+        dispatch(stateEdit());
     }
 
     return (
@@ -26,6 +31,8 @@ const Todos = () => {
                             type={'checkbox'} 
                             checked={item.completed}
                             onChange={()=>handleChangeChecked(item.id)} />
+
+                            <button onClick={()=> handleSelected(item.id, item.title)} disabled={edit} >Edit</button>
                             <button onClick={()=> handleDelete(item.id)}>Delete</button>
                             </li>
                             )
